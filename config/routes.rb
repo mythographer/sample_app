d6e30de3982839d16754ce2e2255dff684e6fa49
Eduard Kasimov <eduard.kasimov@gmail.com>
2016-09-01 13:41:37 +0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
