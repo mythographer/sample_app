1b46d043d76cb170e96fca991cb41e239f759d03
Eduard Kasimov <eduard.kasimov@gmail.com>
2016-09-03 14:07:20 +0300

    Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
     port: 587,
     authentication: :plain,
     user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD']
+    password: ENV['GMAIL_PASSWORD'],
     domain: 'heroku.com',
     enable_starttls_auto: true,
   }
